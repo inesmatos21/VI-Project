3ef8a6a5675abd8d112922b1f2471b2b0025a1e7
Ines Matos <inesgamer2108@gmail.com>
2026-06-02 15:46:25 +0100

    Revert "SceneBuilder corrected"
    This reverts commit 5da8cd6ac720648b8faaed42465cbce4b0b1b00e.

--- src/Primitive/Geometry/Sphere.hpp
+++ src/Primitive/Geometry/Sphere.hpp
@@ -30,8 +30,6 @@ public:
     return m_BoundingBox;
   }
 
-  inline float GetRadius() const { return m_Radius; } 
-
 private:
   // Returns the sphere center at the ray's time (linear interpolation)
   Point CenterAt(float time) const {
